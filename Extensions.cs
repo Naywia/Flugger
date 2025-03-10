a9e501c820589e12e4d1f96faa725ef28cf7181e
Naywia <48431873+Naywia@users.noreply.github.com>
2025-03-10 17:27:25 +0100

    Danishify

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Flügger
+{
+    public static class Extensions
+    {
+        // PropertyInfoExtensions
+        public static string GetDanishName(this PropertyInfo property)
+        {
+            var displayAttr = property.GetCustomAttribute<DisplayAttribute>();
+            return displayAttr?.Name ?? property.Name; // Fall back to original if no attribute
+        }
+
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,18 @@
-﻿namespace Flügger.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flügger.Models
 {
     public class Product
     {
         public string? ImagePath { get; set; }
+
+        [Display(Name = "Varenummer")]
         public int ItemNumber { get; set; }
+
+        [Display(Name = "Navn")]
         public string? Name { get; set; }
+
+        [Display(Name = "Antal")]
         public int Storage { get; set; }
     }
 }
