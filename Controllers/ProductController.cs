de58e248982505d8c537c145e4db4609c4c3f35a
Naywia <48431873+Naywia@users.noreply.github.com>
2025-03-10 14:42:24 +0100

    Update ProductController.cs

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,7 +10,7 @@ namespace Flügger.Controllers
             new() { ImagePath = "~/images/FL2IN1CL.png", ItemNumber = 3, Name = "2-in-1 WoodTex Classic", Storage = 1 },
             new() { ImagePath = "~/images/FLUTEX.png", ItemNumber = 4, Name = "Flutex 7S", Storage = 7 },
             new() { ImagePath = "~/images/H2O.png", ItemNumber = 5, Name = "Væg primer H2O", Storage = 8 },
-            new() { ImagePath = "~/images/sandplast", ItemNumber = 6, Name = "Sandplast LG", Storage = 4 }
+            new() { ImagePath = "~/images/sandplast.png", ItemNumber = 6, Name = "Sandplast LG", Storage = 4 }
         ];
 
         public List<Product> GetProducts()
